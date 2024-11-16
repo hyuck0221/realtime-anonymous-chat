f1d6a4e5fd2bad2986e8631ed98f0f7be677d8e6
심혁 <shimhyuck@simhyeog-ui-MacBookPro.local>
2024-11-16 12:20:58 +0900

    init commit

---
+++ src/main/kotlin/com/hshim/realtimeanonymouschat/RealtimeAnonymousChatApplication.kt
@@ -0,0 +1,20 @@
+package com.hshim.realtimeanonymouschat
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration
+import org.springframework.boot.runApplication
+
+@SpringBootApplication(
+    exclude = [
+        DataSourceAutoConfiguration::class,
+        DataSourceTransactionManagerAutoConfiguration::class,
+        HibernateJpaAutoConfiguration::class
+    ]
+)
+class RealtimeAnonymousChatApplication
+
+fun main(args: Array<String>) {
+    runApplication<RealtimeAnonymousChatApplication>(*args)
+}
---
+++ src/test/kotlin/com/hshim/realtimeanonymouschat/RealtimeAnonymousChatApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hshim.realtimeanonymouschat
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RealtimeAnonymousChatApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
